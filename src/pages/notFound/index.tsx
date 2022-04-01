3d9b3b4fbaac9e41cb28178dae2dadc0567840db
Julian Schlenker <schlenkerjulian@outlook.com>
2022-04-01 22:02:02 +0200

    chore: remove unused imports

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
 import { Spacer } from "../../others/components/Spacer";
-import { Text } from "../../others/components/Text";
 import { Header } from "../../others/components/Header";
 import { Content } from "../../others/components/Content";
 import { NotFound } from "../../others/components/NotFound";
 
 export function NotFoundPage() {
-  const { t } = useTranslation();
-
   return (
     <React.Fragment>
       <Header backLink="/" />
