4baf062b1a8d0365bf86392214b4443b28217162
June Choe <jueunchoe@gmail.com>
2022-04-03 18:09:09 -0700

    dropdown icon change from old to new

--- src/medias/images/UGT_Asset_UI_Dropdown.tsx
+++ src/medias/images/UGT_Asset_UI_Dropdown.tsx
@@ -7,9 +7,22 @@ export interface ImgDropdownProps extends React.SVGProps<SVGSVGElement> {
 
 export const ImgDropdown: React.FunctionComponent<ImgDropdownProps> = ({ alt, fill, ...props }) => {
   return (
-    <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg
+      width="14"
+      height="8"
+      viewBox="0 0 14 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
       {Boolean(alt) && <title>{alt}</title>}
-      <path d="M12 1.41L10.59 -6.16331e-08L6 4.58L1.41 -4.62904e-07L-6.16331e-08 1.41L6 7.41L12 1.41Z" fill={fill ?? "var(--color-secondary)"} />
+      <path
+        d="M13 1L7 7L1 1"
+        stroke="#0D1234"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
     </svg>
   );
 };
