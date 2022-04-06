59553048db0668c8cdab7eaf46ad4ea5d50fce97
AntonRaichuk <raveman.net@gmail.com>
2022-04-06 12:58:51 +0000

    Fix Ukrainian flag

--- src/medias/images/UGT_Asset_FlagSelector_UK.tsx
+++ src/medias/images/UGT_Asset_FlagSelector_UK.tsx
@@ -17,11 +17,11 @@ export const ImgFlagUk: React.FunctionComponent<ImgFlagUkProps> = ({ alt, ...pro
       {Boolean(alt) && <title>{alt}</title>}
       <path
         d="M0 8H20V14C20 15.1046 19.1046 16 18 16H2C0.895431 16 0 15.1046 0 14V8Z"
-        fill="#1337B8"
+        fill="#FFD400"
       />
       <path
         d="M0 2C0 0.895431 0.895431 0 2 0H18C19.1046 0 20 0.895431 20 2V8H0V2Z"
-        fill="#FFD400"
+        fill="#1337B8"
       />
     </svg>
   );
