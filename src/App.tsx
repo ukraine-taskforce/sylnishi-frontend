1fd1c9df5c519334602cba3bad4201b2c1fa944f
Julian Schlenker <js99@mail.uni-paderborn.com>
2022-04-17 21:45:09 +0200

    chore: Remove relative imports from App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import { queryClient } from "@contexts/api";
+import { CountryDetailPage } from "@pages/landing/CountryDetail/CountryDetailPage";
+import { CountrySelectionPage } from "@pages/landing/CountrySelection/CountrySelectionPage";
+import { NotFoundPage } from "@pages/shared/NotFound/NotFoundPage";
 import React from "react";
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClientProvider } from "react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { CountrySelectionPage } from "./pages/landing/CountrySelection/CountrySelectionPage";
-import { queryClient } from "./contexts/api";
-import { NotFoundPage } from "./pages/shared/NotFound/NotFoundPage";
-import { CountryDetailPage } from "./pages/landing/CountryDetail/CountryDetailPage";
+
 
 export function App() {
   return (
